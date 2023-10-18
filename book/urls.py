034b5414c5b40e185b4a64a42e6b5d7363507933
Andy <andy77andy@ukr.net>
2023-10-18 23:36:59 +0300

    Added views and urls in corresponding files for app book

---
+++ book/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from book.views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet)
+
+urlpatterns = router.urls
+
+app_name = "book"
